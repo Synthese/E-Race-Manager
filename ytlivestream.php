987802b530e617d209519fa1bb4906797a1b18bb
Oliver Haase <ollysun@web.de>
2019-05-29 20:46:27 +0200

    Add files via upload

---
+++ ytlivestream.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * +-------------------------------------------------------+
+ * | E - Race - Manager
+ * | PHP-Liga Management System
+ * | Copyright (C) Synthese
+ * | https://www.e-race-manager.all-webservice.de/
+ * +-------------------------------------------------------+
+ * | Filename: ytlivestream.php
+ * | Version : 1.1.03.0
+ * | Datum   : 05.05.2019
+ * | Author  : Synthese 
+ * +-------------------------------------------------------+
+ * | Entfernung von diesem
+ * | Copyright-Header ist streng verboten ohne
+ * | schriftliche Genehmigung des ursprünglichen Autors.
+ * |
+ * +-------------------------------------------------------+
+ */
+
+include ("head.php");
+?>
+		<div class="container">
+				<div class="row">
+					<div class="col col-lg-12">
+									<?php include 'navi.php';?>
+					</div>
+				</div>
+		</div>
+	<div class="container-fluid">
+			<div class="text-center">
+				<!--Edit Sidebar Content here-->
+				<div class="col col-lg-12">
+					<h3>Youtube Livestream</h3>
+					<p>Abonniere unseren Kanal</p>
+					<br />
+   </div>
+				<!--Edit Main Content Area here-->
+				<div class="col col-lg-12">
+						<iframe width="1280" height="720"
+							src="http://www.youtube.com/embed/live_stream?channel=UC1eV7wV3RiBbySxnsxwqXjw"; 
+							  frameborder="0" allowfullscreen></iframe>
+				</div>
+				<!--/End Main Content Area here-->
+			</div>
+	</div>
+	<div>
+<?php include 'footer.php';?>
+</div>
