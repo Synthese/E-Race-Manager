b62429c55016f44013757358a6744f73a1e32e2e
Oliver Haase <ollysun@web.de>
2019-05-29 20:52:06 +0200

    Add files via upload

---
+++ twlivestream.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * -------------------------------------------------------+
+ * | PHP-Liga Management System E-Race_Manager
+ * | Copyright (C) Synthes
+ * | https://www.e-race-manager.all-webservice.de/
+ * +--------------------------------------------------------+
+ * | Filename: twlivestream.php
+ * | Author: Synthese
+ * | Datum : 22.05.2019
+ * +--------------------------------------------------------+
+ * | Entfernung von diesem
+ * | Copyright-Header ist strengstens verboten ohne
+ * | schriftliche Genehmigung des Autors.
+ * |
+ * | Toni Vicente (arv187), Pablo Oña (inguni), 
+ * | Stefan Meissner (stmeissner) sind ursprüngliche Autoren 
+ * | von PREM Podium Rennen E Manager sowie Autor 
+ * | Bert Hekman (DemonTPX) der ursprünglicher Autor von 
+ * | Paddock 7.10beta war.
+ * | 
+ * +--------------------------------------------------------
+ */
+
+include ("head.php");
+?>
+
+
+
+		<div class="container">
+				<div class="row">
+					<div class="col col-lg-12">
+									<?php include 'navi.php';?>
+					</div>
+				</div>
+		</div>
+
+	<div class="container-fluid">
+			<div class="text-center">
+				<div class="col col-lg-12">
+					  <h3>Live bei Twitch</h3>
+				  	<p>
+						Ihr könnt uns Live erleben auf Twitch. Dazu schaut einfach nach ob
+						wir das Rennen oder die Veranstaltung auch auf Twitch streamen.<br />Dazu
+						schaut in unseren Kalender wir tragen das dort auch ein wann wir
+						wo streamen.
+				 	</p>
+				<!--Edit Main Content Area here-->
+				<div class="col col-lg-12">
+					
+						<iframe width="1280" height="720"
+							src="https://player.twitch.tv/?channel=synth_d"; fallowfullscreen="true" scrolling="no">
+							</iframe>
+				</div>
+
+
+	    </div>
+			 </div>
+  </div>
+	<div>
+<?php include 'footer.php';?>
+</div>
