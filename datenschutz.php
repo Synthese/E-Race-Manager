67d5ffbcbc124bc012df896d52f7df0a6e3cf101
Oliver Haase <ollysun@web.de>
2019-05-30 16:36:50 +0200

    Add files via upload

---
+++ datenschutz.php
@@ -0,0 +1,16 @@
+
+<div>&nbsp;</div>
+<div class="container">
+	<div class="card">
+		<div class="card-header">
+			<b>Datenschutzerklärung</b>
+		</div><small>
+		<p>Hier die Datenschutzerklärung einfügen
+			<br />
+		</p>
+
+
+		
+</small>
+	</div>
+</div>
