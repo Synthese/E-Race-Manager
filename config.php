d54ebc8ae1e5195ac04993c60e8911b258aa2a82
Oliver Haase <ollysun@web.de>
2019-05-30 20:53:09 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define("CONFIG", true);
 # -------------------------------------------------------
 
 # Organisation title. This is the linked title that will appear on top of the page.
-$config['org'] = "E-Race-Liga";
+$config['org'] = "E-Race-Manager";
 
 # Organisation link. This will be where the link points to.
 $config['org_link'] = "http://webseite.de";
@@ -16,7 +16,7 @@ $config['mysql']['pass'] = "*****";
 $config['mysql']['db'] = "deineDatenbank";
 $config['mysql']['port'] = "3306";
 
-
+//Dieser Bereich ist noch nicht aktive gesetzt und wird noch geändert
 $praefix ="rc_";                                // Tabellen Präfix für weitere Installationen
 $einstellungen =    $praefix."einstellungen";       // Tabelle für Einstellungen
 $news = $praefix."news"; // Tabelle für News
