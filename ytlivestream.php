1eaef29770b00c827f9b2f75909b9e8b0dd48f2c
Oliver Haase <ollysun@web.de>
2019-08-20 16:56:39 +0200

    Update ytlivestream.php

--- ytlivestream.php
+++ ytlivestream.php
@@ -7,8 +7,8 @@
  * | https://www.e-race-manager.all-webservice.de/
  * +-------------------------------------------------------+
  * | Filename: ytlivestream.php
- * | Version : 1.1.03.0
- * | Datum   : 05.05.2019
+ * | Version : 1.1.04.1
+ * | Datum   : 20.08.2019
  * | Author  : Synthese 
  * +-------------------------------------------------------+
  * | Entfernung von diesem
@@ -18,32 +18,29 @@
  * +-------------------------------------------------------+
  */
 
-include ("head.php");
+include ("theme/head.php");
 ?>
-		<div class="container">
-				<div class="row">
-					<div class="col col-lg-12">
-									<?php include 'navi.php';?>
-					</div>
-				</div>
-		</div>
-	<div class="container-fluid">
-			<div class="text-center">
-				<!--Edit Sidebar Content here-->
-				<div class="col col-lg-12">
-					<h3>Youtube Livestream</h3>
-					<p>Abonniere unseren Kanal</p>
-					<br />
-   </div>
-				<!--Edit Main Content Area here-->
-				<div class="col col-lg-12">
-						<iframe width="1280" height="720"
-							src="http://www.youtube.com/embed/live_stream?channel=Deine ID"; 
-							  frameborder="0" allowfullscreen></iframe>
-				</div>
-				<!--/End Main Content Area here-->
-			</div>
-	</div>
-	<div>
-<?php include 'footer.php';?>
+<div class="container">
+    <div class="row">
+      <div class="col col-lg-12">
+			<?php include 'theme/navi.php';?>
+      </div>
+    </div>
 </div>
+<div class="container-fluid">
+    <div class="text-center">
+	<!--Edit Sidebar Content here-->
+       <div class="col col-lg-12">
+		<h3>Youtube Livestream</h3>
+		<p>Abonniere unseren Kanal</p>
+		<br />
+      </div>
+<!--Edit Main Content Area here-->
+     <div class="col col-lg-12">
+  <iframe width="1280" height="720" 
+	  src="http://www.youtube.com/embed/live_stream?channel=Deine ID" frameborder="0" allowfullscreen></iframe>
+    </div>
+<!--/End Main Content Area here-->
+       </div>
+    </div>
+<div><?php include 'theme/footer.php';?></div>
